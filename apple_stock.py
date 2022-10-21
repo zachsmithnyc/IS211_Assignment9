762d529d36c0bd69697db5181ea9ebfdde20356c
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-10-21 18:44:44 -0400

    assingment 9

--- apple_stock.py
+++ apple_stock.py
@@ -16,23 +16,23 @@ def download(url):
 
 
 if __name__ == "__main__":
-    wiki_url = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL&guccounter=1"
+    wiki_url = "https://www.macrotrends.net/stocks/charts/AAPL/apple/stock-price-history"
     url_text = download(wiki_url)
 
     # Create a BeatifulSoup object
     soup = BeautifulSoup(url_text, features="lxml")
 
-    result_table = soup.find_all('table', class_="W(100%) M(0)")
-    print(result_table)
+    result_table = soup.find_all('table', class_='historical_data_table')
     rows = result_table[0].find_all('tr')
-    headers = rows[0].find_all('th')
+    headers = rows[1].find_all('th')
+
     print(
-        f"{headers[0].text.strip():<15} - {headers[1].text.strip():<40} - {headers[2].text.strip():<20} - {headers[3].text.strip():<15} - {headers[4].text.strip():<15}"
+        f"{headers[0].text.strip():<10} - {headers[1].text.strip():<15} - {headers[2].text.strip():<15} - {headers[3].text.strip():<15} - {headers[4].text.strip():<15} - {headers[5].text.strip():<15}"
     )
     for row in rows:
         cells = row.find_all('td')
         if not cells:
             continue
         print(
-            f"{cells[0].text.strip():<15} - {cells[1].text.strip():<15} - {cells[2].text.strip():<15} - {cells[3].text.strip():<15} - {cells[4].text.strip():<15}"
+            f"{cells[0].text.strip():<15} - {cells[1].text.strip():<15} - {cells[2].text.strip():<15} - {cells[3].text.strip():<15} - {cells[4].text.strip():<15} - {cells[5].text.strip():<15}"
         )
